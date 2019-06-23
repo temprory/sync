0ef9c99a5ef3dc0a904c73c2ef8302ab2a7198a0
temprory <beishengren@126.com>
2019-06-23 22:21:17 +0800

    -

--- waitsession.go
+++ waitsession.go
@@ -11,75 +11,68 @@ type WaitSession struct {
 	sessions map[interface{}]chan interface{}
 }
 
-func (mgr *WaitSession) Add(sess interface{}) error {
-	mgr.Lock()
-	if mgr.sessions == nil {
-		mgr.sessions = map[interface{}]chan interface{}{}
+func (ws *WaitSession) Add(sess interface{}) error {
+	ws.Lock()
+	if ws.sessions == nil {
+		ws.sessions = map[interface{}]chan interface{}{}
 	}
-	if _, ok := mgr.sessions[sess]; ok {
-		mgr.Unlock()
+	if _, ok := ws.sessions[sess]; ok {
+		ws.Unlock()
 		return fmt.Errorf("session %v exist", sess)
 	}
-	mgr.sessions[sess] = make(chan interface{}, 1)
-	mgr.Unlock()
+	ws.sessions[sess] = make(chan interface{}, 1)
+	ws.Unlock()
 
 	return nil
 }
 
-func (mgr *WaitSession) Done(sess interface{}, data interface{}) error {
-	mgr.Lock()
-	if done, ok := mgr.sessions[sess]; ok {
-		//delete(mgr.sessions, sess)
-		mgr.Unlock()
+func (ws *WaitSession) Done(sess interface{}, data interface{}) error {
+	ws.Lock()
+	if done, ok := ws.sessions[sess]; ok {
+		ws.Unlock()
 		done <- data
 		return nil
 	}
-	mgr.Unlock()
+	ws.Unlock()
 
 	return fmt.Errorf("session %v not exist", sess)
 }
 
-func (mgr *WaitSession) Wait(sess interface{}, timeout time.Duration) (interface{}, error) {
-	mgr.Lock()
-	done, ok := mgr.sessions[sess]
+func (ws *WaitSession) Wait(sess interface{}, timeout time.Duration) (interface{}, error) {
+	ws.Lock()
+	done, ok := ws.sessions[sess]
 	if ok {
-		mgr.Unlock()
+		ws.Unlock()
 
 		var data interface{}
 		if timeout > 0 {
 			select {
 			case data = <-done:
 			case <-time.After(timeout):
-				mgr.Lock()
-				delete(mgr.sessions, sess)
-				mgr.Unlock()
+				ws.Lock()
+				delete(ws.sessions, sess)
+				ws.Unlock()
 				return nil, fmt.Errorf("wait session %v timeout", sess)
 			}
 		} else {
 			data = <-done
 		}
 
-		mgr.Lock()
-		delete(mgr.sessions, sess)
-		mgr.Unlock()
+		ws.Lock()
+		delete(ws.sessions, sess)
+		ws.Unlock()
 
 		return data, nil
 	}
 
-	mgr.Unlock()
+	ws.Unlock()
 
 	return nil, fmt.Errorf("session %v not exist", sess)
 }
 
-func (mgr *WaitSession) Len() int {
-	mgr.Lock()
-	l := len(mgr.sessions)
-	mgr.Unlock()
+func (ws *WaitSession) Len() int {
+	ws.Lock()
+	l := len(ws.sessions)
+	ws.Unlock()
 	return l
 }
-
-// func NewWaitSession() *WaitSession {
-// 	return &WaitSession{
-// 		sessions: map[interface{}]chan interface{}{},
-// 	}
-// }
